788add1e842979e6beb7656eafcf7affe5e36b0d
Tiaan Louw <tiaanl@gmail.com>
2022-06-14 20:56:08 +0200

    Adding a lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Token {
     Constant(i32),
     Plus,
@@ -16,5 +16,66 @@ impl Token {
 
             Token::Constant(_) | Token::End => unreachable!(),
         }
+    }
+}
+
+pub struct Lexer<'a> {
+    source: &'a str,
+    pos: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Self { source, pos: 0 }
+    }
+
+    fn next_token(&mut self) -> Token {
+        let first = match self.source[self.pos..].chars().next() {
+            Some(c) => c,
+            None => return Token::End,
+        };
+
+        dbg!(first);
+
+        match first {
+            c if c.is_numeric() => self.consume_constant(),
+
+            _ => todo!(),
+        }
+    }
+
+    fn consume_constant(&mut self) -> Token {
+        let start = self.pos;
+
+        match self.source[start..].find(|c: char| !c.is_numeric()) {
+            Some(found) => self.pos += dbg!(found),
+            None => self.pos = self.source.len(),
+        };
+
+        dbg!(&self.source[start..self.pos]);
+
+        Token::Constant(self.source[start..self.pos].parse().unwrap())
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.next_token() {
+            Token::End => None,
+            t => Some(t),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        let lexer = Lexer::new("10");
+        dbg!(lexer.collect::<Vec<Token>>());
     }
 }
